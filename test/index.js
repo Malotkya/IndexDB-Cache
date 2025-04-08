01f8e2d59786ac4f53c21631664abe7d5f79420c
Malotkya <47186158+Malotkya@users.noreply.github.com>
2025-04-08 11:04:05 -0500

    Clear indexDB before testing.

--- test/index.js
+++ test/index.js
@@ -1,15 +1,27 @@
 import Cache from "../build/index.js";
 import {test} from "./test.js";
 
+(async()=>{
+    for(const name of await indexedDB.databases()) {
+        indexedDB.deleteDatabase(name);
+    }
 
-test("Init Cache Test", async()=>{
-    const cache = await Cache("Test");
-    cache.close();
-});
+    test("Init Cache Test", async()=>{
+        const cache = await Cache("Test");
+        cache.close();
+    });
+    
+    test("Get/Set Value Test", async()=>{
+        const cache = await Cache("Test");
+        await cache.set("Key", {message: "Hello World!"})
+        const result = await cache.get("Key");
+        return result.message;
+    });
+    
+    test("Second Store", async()=>{
+        const cache = await Cache("Second");
+        cache.set(12, "Successful!");
+    });
+    
+})()
 
-test("Get/Set Value Test", async()=>{
-    const cache = await Cache("Test");
-    await cache.set("Key", {message: "Hello World!"})
-    const result = await cache.get("Key");
-    return result.message;
-});
